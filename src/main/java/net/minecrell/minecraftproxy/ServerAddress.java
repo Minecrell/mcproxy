ab9b1223bacb9e41b08718b64600d2b389d20fc9
Minecrell <dev@minecrell.net>
2014-01-08 20:56:17 +0100

    Add license headers.

--- src/main/java/net/minecrell/minecraftproxy/ServerAddress.java
+++ src/main/java/net/minecrell/minecraftproxy/ServerAddress.java
@@ -1,3 +1,17 @@
+/*
+ * MinecraftProxy - TCP-Proxy for Minecraft servers.
+ * Written in 2013 by Minecrell <https://github.com/Minecrell>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide.
+ *
+ * This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 package net.minecrell.minecraftproxy;
 
 import lombok.Value;
