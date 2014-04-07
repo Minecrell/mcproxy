cfd6a1afd286d9baea106d7211a55795039affa2
Minecrell <dev@minecrell.net>
2014-04-07 19:53:13 +0200

    Create a Go version

--- src/main/java/net/minecrell/minecraftproxy/util/Closeable.java
+++
@@ -1,5 +0,0 @@
-package net.minecrell.minecraftproxy.util;
-
-public interface Closeable {
-    void close();
-}
--- src/main/java/net/minecrell/minecraftproxy/util/Startable.java
+++
@@ -1,6 +0,0 @@
-package net.minecrell.minecraftproxy.util;
-
-public interface Startable {
-    boolean start();
-    boolean isStarted();
-}
