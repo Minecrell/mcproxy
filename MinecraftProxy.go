2a48c84bd8be6395bf221eb4e4cd2be29c7428d1
Minecrell <dev@minecrell.net>
2014-04-09 20:05:04 +0200

    Make transforming optional.

--- MinecraftProxy.go
+++ MinecraftProxy.go
@@ -11,11 +11,11 @@ import (
 )
 
 func badUsage() {
-	fmt.Fprintln(os.Stderr, "Usage: MinecraftProxy <Host:Port> <RemoteHost:RemotePort>"); os.Exit(1)
+	fmt.Fprintln(os.Stderr, "Usage: MinecraftProxy <Host:Port> <RemoteHost:RemotePort> [Version]"); os.Exit(1)
 }
 
 func Create() {
-	if len(os.Args) < 2  || len(os.Args) > 3 { badUsage() }
+	if len(os.Args) < 2  || len(os.Args) > 4 { badUsage() }
 
 	localHost := ParseAddress(os.Args[1])
 	if localHost == nil { badUsage() }
@@ -27,26 +27,33 @@ func Create() {
 	_, _, err = remoteHost.Resolve()
 	if err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) }
 
+	version := ""
+	if len(os.Args) > 3 { version = os.Args[3] }
+
 	fmt.Println("Listening to:", os.Args[1])
 	fmt.Println("Proxying to:", os.Args[2])
 	fmt.Println()
 
 	log.SetPrefix("[MinecraftProxy] ")
 	log.SetOutput(os.Stdout)
-	Start(localAddress, remoteHost)
+	err = Start(localAddress, remoteHost, version)
+	if err != nil {
+		log.Fatalln("Unable to start proxy:", err.Error())
+	}
 }
 
-func Start(localAddress *net.TCPAddr, remoteAddress *ServerAddress) (err error) {
+func Start(localAddress *net.TCPAddr, remoteAddress *ServerAddress, version string) (err error) {
 	local, err := net.ListenTCP("tcp", localAddress)
 	if err != nil { return }
 	defer local.Close()
 
 	log.Println("Network listener started and is waiting for requests.")
+	if version == "1.7" { log.Println("Transforming handshakes for version 1.7") }
 
 	for {
 		conn, err := local.AcceptTCP()
 		if err == nil {
-			go proxy(conn, remoteAddress)
+			go proxy(conn, remoteAddress, version)
 		} else {
 			log.Println("Connection failed:", err)
 		}
@@ -76,7 +83,7 @@ func transformHandshake(local *net.TCPConn, remote *net.TCPConn, address *Connec
 	_, err = remote.Write(writer.Bytes()); return
 }
 
-func proxy(local *net.TCPConn, remoteHost *ServerAddress) {
+func proxy(local *net.TCPConn, remoteHost *ServerAddress, version string) {
 	defer local.Close()
 	remoteAddress, connectAddress, err := remoteHost.Resolve()
 	if err != nil { log.Println("Cannot resolve address:", err.Error()); return }
@@ -87,8 +94,10 @@ func proxy(local *net.TCPConn, remoteHost *ServerAddress) {
 	if err != nil { log.Println("Unable to connect:", err); return }
 	defer remote.Close()
 
-	err = transformHandshake(local, remote, connectAddress)
-	if err != nil { log.Println("Unable to transform handshake:", err)}
+	if version == "1.7" {
+		err = transformHandshake(local, remote, connectAddress)
+		if err != nil { log.Println("Unable to transform handshake:", err)}
+	}
 
 	copyError := make(chan string)
 	go copyStream(local, remote, copyError, true)
